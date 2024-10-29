33a6d8e834b28743e9be228f68087a31644404d3
Jorge Luis Aguilar Rodarte <jorgeluis_942009@hotmail.com>
2024-10-29 16:16:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ import { createServer } from 'node:http';
 import cors from 'cors';
 import helmet from 'helmet';
 import { Server } from 'socket.io';
+import 'dotenv/config';
 
 const app = express();
 app.use(cors({ origin: process.env.REACT_APP_CLIENT_HOST }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-if (process.env.NODE_ENV === 'production') require('dotenv').config();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: process.env.REACT_APP_CLIENT_HOST }});
 const PORT = process.env.REACT_APP_SERVER_PORT;
